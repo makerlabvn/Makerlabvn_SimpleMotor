66e854ca47fca818a6a76871516e815b53828ebb
MinhKhanh614 <167750602+MinhKhanh614@users.noreply.github.com>
2025-02-13 17:17:34 +0700

    Add support for six-pin motor control and enhance motor library functionality

--- src/Makerlabvn_SimpleMotor.h
+++ src/Makerlabvn_SimpleMotor.h
@@ -17,34 +17,34 @@
 /*                                   CLASS                                   */
 /* ------------------------------------------------------------------------- */
 
-class Makerlabvn_SimpleMotor
-{
+class Makerlabvn_SimpleMotor {
 public:
   /* ------------------------------- HÀM TẠO ------------------------------- */
+  Makerlabvn_SimpleMotor();
   Makerlabvn_SimpleMotor(
-      int pinIn1, int pinIn2,
-      int pinIn3, int pinIn4);
+    int pinIn1, int pinIn2,
+    int pinIn3, int pinIn4);
 
   /* ------------------------- HÀM ĐIỀU KHIỂN MOTOR ------------------------ */
 
-  void motorA_fw(int speed); // Điều khiển motor kênh A quay thuận
-  void motorB_fw(int speed); // Điều khiển motor kênh B quay thuận
+  void motorA_fw(int speed);  // Điều khiển motor kênh A quay thuận
+  void motorB_fw(int speed);  // Điều khiển motor kênh B quay thuận
 
-  void motorA_bw(int speed); // Điều khiển motor kênh A quay ngược
-  void motorB_bw(int speed); // Điều khiển motor kênh B quay ngược
+  void motorA_bw(int speed);  // Điều khiển motor kênh A quay ngược
+  void motorB_bw(int speed);  // Điều khiển motor kênh B quay ngược
 
-  void motorA_stop(); // Điều khiển motor kênh A dừng lại
-  void motorB_stop(); // Điều khiển motor kênh B dừng lại
+  void motorA_stop();  // Điều khiển motor kênh A dừng lại
+  void motorB_stop();  // Điều khiển motor kênh B dừng lại
 
   /* -------------------------- HÀM ĐIỀU KHIỂN XE -------------------------- */
 
-  void car_fw(int speedA, int speedB); // Điều khiển xe đi tới
-  void car_bw(int speedA, int speedB); // Điều khiển xe đi lùi
+  void car_fw(int speedA, int speedB);  // Điều khiển xe đi tới
+  void car_bw(int speedA, int speedB);  // Điều khiển xe đi lùi
 
-  void car_rotateL(int speed); // Điều khiển xe xoay trái
-  void car_rotateR(int speed); // Điều khiển xe xoay phải
+  void car_rotateL(int speed);  // Điều khiển xe xoay trái
+  void car_rotateR(int speed);  // Điều khiển xe xoay phải
 
-  void car_stop(); // Điều khiển xe dừng lại
+  void car_stop();  // Điều khiển xe dừng lại
 
 private:
   /* ----------------------- (BIẾN) CẤU HÌNH CÁC PIN ----------------------- */
@@ -62,6 +62,74 @@ private:
   int calculate_speed(int speed);
 };
 
+class Makerlabvn_SimpleMotorSixPins {
+public:
+  /* ------------------------------- HÀM TẠO ------------------------------- */
+  Makerlabvn_SimpleMotorSixPins();
+  
+  void setup(int pinIN1,
+             int pinIN2,
+             int pinIN3,
+             int pinIN4,
+             int pinENA,
+             int pinENB);
+
+  /* ------------------------- HÀM ĐIỀU KHIỂN MOTOR ------------------------ */
+
+  void motorA_fw(int speed);  // Điều khiển motor kênh A quay thuận
+  void motorB_fw(int speed);  // Điều khiển motor kênh B quay thuận
+
+  void motorA_bw(int speed);  // Điều khiển motor kênh A quay ngược
+  void motorB_bw(int speed);  // Điều khiển motor kênh B quay ngược
+
+  void motorA_stop();  // Điều khiển motor kênh A dừng lại
+  void motorB_stop();  // Điều khiển motor kênh B dừng lại
+
+  /* -------------------------- HÀM ĐIỀU KHIỂN XE -------------------------- */
+
+  void car_fw(int speedA, int speedB);  // Điều khiển xe đi tới
+  void car_bw(int speedA, int speedB);  // Điều khiển xe đi lùi
+
+  void car_rotateL(int speed);  // Điều khiển xe xoay trái
+  void car_rotateR(int speed);  // Điều khiển xe xoay phải
+
+  void car_stop();  // Điều khiển xe dừng lại
+
+private:
+  /* ----------------------- (BIẾN) CẤU HÌNH CÁC PIN ----------------------- */
+  int _pinIn1;
+  int _pinIn2;
+  int _pinIn3;
+  int _pinIn4;
+  int _pinENA;
+  int _pinENB;
+
+  /* --------------------------- (HÀM) ĐẶT TỐC ĐỘ -------------------------- */
+
+  /**
+   * Đảm bảo giá trị tốc độ (%) nhận được trong khoảng 0% đến 100%
+   * Đồng thời chuyển đổi tốc độ (%) sang thang (PWM) tương ứng
+   */
+  int calculate_speed(int speed);
+};
+
 /* ------------------------------------------------------------------------- */
+class Motor_lib{
+  public:
+  uint8_t in1;
+  uint8_t in2;
+  uint8_t pwm;
+  unsigned long lastTime, timeRun;
+
+  //motor(int);
+  void begin(int pwm_, int in1_, int in2_);
+  void forward(int iSpeed);
+  void backward(int iSpeed);
+  void stop();
+  void setTimeRun(unsigned long ul_timerun_);
+  void checkStop();
+  void resetTimeRun();
+  
+};
 
 #endif
